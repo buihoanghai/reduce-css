1881db9aeaa1386efd4d600407f715d96fe34d9d
Hai Bui <hoanghaibui2003@gmail.com>
2018-02-28 15:53:00 +0800

    Revert "Create mixin"

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-﻿var CSSReader = require('/CSSReader');
-function process() {
-    var rawStr = $('#rawStr').val();
-    var cssReader = new CSSReader();
-    var nodes = cssReader.parse(rawStr);
-    var result = "";
-    for(var i = 0; i < nodes.length; i++) {
-        var mixinStr = nodes[i].createMixin();
-        result = `${result}
-        ${mixinStr}`;
-    }
-    $('#processedStr').val(result);
-}
+﻿
--- libraries/CSSNode.js
+++ libraries/CSSNode.js
@@ -1,43 +1,71 @@
-var Attribute = require('./Attribute');
+
+// /*! normalize.css v7.0.0 | MIT License | github.com/necolas/normalize.css */
+//
+// /* Document
+//    ========================================================================== */
+//
+// /**
+//  * 1. Correct the line height in all browsers.
+//  * 2. Prevent adjustments of font size after orientation changes in
+//  *    IE on Windows Phone and in iOS.
+//  */
+//
+// html {
+//     line-height: 1.15; /* 1 */
+//     -ms-text-size-adjust: 100%; /* 2 */
+//     -webkit-text-size-adjust: 100%; /* 2 */
+// }
+//
+
+
+// /* Sections
+//    ========================================================================== */
+//
+// /**
+//  * Remove the margin in all browsers (opinionated).
+//  */
+//
+// body {
+//     margin: 0;
+// }
+//
+// /**
+//  * Add the correct display in IE 9-.
+//  */
+//
+
+
+// article,
+//     aside,
+//     footer,
+//     header,
+//     nav,
+//     section {
+//     display: block;
+// }
+//
+
+
+// /**
+//  * Correct the font size and margin on `h1` elements within `section` and
+//  * `article` contexts in Chrome, Firefox, and Safari.
+//  */
+//
+// h1 {
+//     font-size: 2em;
+//     margin: 0.67em 0;
+// }
+
+// @media (--breakpoint-not-small) {
+// .medium-and-larger-specific-style {
+//         background-color: red;
+//     }
+// }
 function CSSNode(query, attributes, mediaQuery, comment) {
     this.query = query.removeComment().trim();
     this.attributes = attributes;
-    if(mediaQuery){
-        mediaQuery = mediaQuery.replace("screen and (min-width: 30em) and (max-width: 60em)", "screen and (min-width: 30em) and (max-width: 64em)")
-            .replace("screen and (min-width: 60em)", "screen and (min-width: 64em)");
-    }
     this.mediaQuery = (mediaQuery || "").trim();
     this.comment = (comment || "").trim();
-    // @mixin breakpoint-down($point) {
-    //     @media ('max-width': $point) {
-    //         @content;
-    //         }
-    //     }
-    this.createMixin = function() {
-        var nameMixin = this.getMixinName();
-        var mediaQuery = this.mediaQuery;
-        var attributes = this.attributes;
-        var result = `@mixin ${nameMixin}{`;
-        if (mediaQuery) {
-            result = `${result} @media ${mediaQuery}{`;
-        }
-        result = `${result}${this.query}{`
-        for( var i = 0; i<attributes.length; i++) {
-            var attribute = attributes[i];
-
-            result = `${result}${attribute.name}:${attribute.value};`;
-        }
-        result = result + '}';
-        if(mediaQuery){
-            result = result + '}';
-        }
-        result = result + '}';
-        return result;
-
-    }
-    this.getMixinName = function() {
-        return "tachyon-" + this.query.replace(/[&\/\\#,+()$~%.'":*?<>{} =\[\]]/g,'');
-    }
 }
 
 CSSNode.parse = function (str, mediaQuery, comment) {
@@ -45,7 +73,13 @@ CSSNode.parse = function (str, mediaQuery, comment) {
     var arr = str.split('{');
     var query = arr[0];
     var queryArr = query.split(',');
-    var attributesRaw = arr[1].replace('}', '');
+    try {
+        var attributesRaw = arr[1].replace('}', '');
+    }
+    catch(err) {
+        console.log(str);
+    }
+
     var attributesArr = attributesRaw.split(';');
     var attributes = [];
     for (let i = 0; i < attributesArr.length; i++) {
@@ -61,5 +95,17 @@ CSSNode.parse = function (str, mediaQuery, comment) {
     }
     return result;
 };
+function Attribute(name, value) {
+    this.name = name.removeComment().trim();
+    this.value = value.trim();
+}
+Attribute.parse = function (str) {
+    var attr = str.split(':');
+    if (attr.length === 2) {
+        return new Attribute(attr[0], attr[1]);
+    } else {
+        return;
+    }
 
+};
 module.exports = CSSNode;
