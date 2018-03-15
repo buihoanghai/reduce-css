0d2a7945d00e7573f85f18223a421a03147ea180
haibui <hoanghaibui2003@gmail.com>
2018-03-15 16:59:54 +0800

    Temp solution

---
+++ dist/temp/CSSNode.js
@@ -0,0 +1,83 @@
+define(["module", './Attribute'], function (module, Attribute) {
+    "use strict";
+
+    function CSSNode(query, attributes, mediaQuery, comment) {
+        this.query = query.removeComment().trim();
+        this.attributes = attributes;
+        if (mediaQuery) {
+            mediaQuery = mediaQuery.replace("screen and (min-width: 30em) and (max-width: 60em)", "screen and (min-width: 30em) and (max-width: 64em)").replace("screen and (min-width: 60em)", "screen and (min-width: 64em)");
+        }
+        this.mediaQuery = (mediaQuery || "").trim();
+        this.comment = (comment || "").trim();
+        // @mixin breakpoint-down($point) {
+        //     @media ('max-width': $point) {
+        //         @content;
+        //         }
+        //     }
+        this.createMixin = function () {
+            var nameMixin = this.getMixinName();
+            var mediaQuery = this.mediaQuery;
+            var attributes = this.attributes;
+            var result = "@mixin " + nameMixin + "{";
+            if (mediaQuery) {
+                result = result + " @media " + mediaQuery + "{";
+            }
+            result = "" + result + this.query + "{";
+            for (var i = 0; i < attributes.length; i++) {
+                var attribute = attributes[i];
+
+                if(attribute.name.indexOf("color")>-1 || attribute.value.indexOf("color")>-1) {
+                    return "";
+                }
+                result = "" + result + attribute.name + ":" + attribute.value + ";";
+            }
+            result = result + '}';
+            if (mediaQuery) {
+                result = result + '}';
+            }
+            result = result + '}';
+            return result;
+        };
+        this.getMixinName = function () {
+            return "tachyon-" + this.query.replace(/[&\/\\#,+()$~%.'":*?<>{} =\[\]]/g, '');
+        };
+        this.getCSS =function() {
+          var attributes = this.attributes;
+          var result = this.query + "{";
+          for (var i = 0; i < attributes.length; i++) {
+            var attribute = attributes[i];
+
+            if (attribute.name.indexOf("color") > -1 || attribute.value.indexOf("color") > -1) {
+              return "";
+            }
+            result = "" + result + attribute.name + ":" + attribute.value + ";";
+          }
+          result = result + '}';
+          return result;
+        }
+    }
+
+    CSSNode.parse = function (str, mediaQuery, comment) {
+        var result = [];
+        var arr = str.split('{');
+        var query = arr[0];
+        var queryArr = query.split(',');
+        var attributesRaw = arr[1].replace('}', '');
+        var attributesArr = attributesRaw.split(';');
+        var attributes = [];
+        for (var i = 0; i < attributesArr.length; i++) {
+            var attr = Attribute.parse(attributesArr[i]);
+            if (attr) {
+                attributes.push(attr);
+            }
+        }
+        for (var _i = 0; _i < queryArr.length; _i++) {
+            if (queryArr[_i]) {
+                result.push(new CSSNode(queryArr[_i], attributes, mediaQuery, comment));
+            }
+        }
+        return result;
+    };
+
+    module.exports = CSSNode;
+});
--- dist/temp/index.js
+++ dist/temp/index.js
@@ -17,17 +17,33 @@ define(['./CSSReader', './ClassReader'], function (CSSReader,ClassReader) {
     var usedClass = classReader.parse(htmlStr);
     var result = "";
     for (var i = 0; i < nodes.length; i++) {
+      //Create mixin
       // var mixinStr = nodes[i].createMixin();
-      // result = result + '\n        ' + mixinStr;
+      //   if(mixinStr)
+      //     result = result + '\n' + mixinStr;
 
-      //
+      //Create example
+      // var mixinStr = nodes[i].getMixinName();
+      // result = result + '\n@include ' + mixinStr + ';';
+
+      //Find used
+      // for (var j = 0; j < usedClass.length; j++) {
+      //   if (nodes[i].query === "." + usedClass[j]) {
+      //     var mixinStr = nodes[i].getMixinName();
+      //     result = result + '\n@include ' + mixinStr + ';';
+      //     break;
+      //   }
+      // }
+
+      //Find used CSS
       for (var j = 0; j < usedClass.length; j++) {
         if (nodes[i].query === "." + usedClass[j]) {
-          var mixinStr = nodes[i].getMixinName();
-          result = result + '\n@include ' + mixinStr + ';';
+          var css = nodes[i].getCSS();
+          result = result + '\n' + css;
           break;
         }
       }
+
     }
     $('#processedStr').val(result);
   }
